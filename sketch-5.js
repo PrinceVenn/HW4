76f1e54449ca90feb434f22577841b1dd3533343
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-10-01 16:24:33 -0700

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,17 @@
+noFill();
+var a = 10
+var b = 175
+var c = 180
+
+while (c < width){
+line (b,a,c,a)
+var a = a+10;
+var b = b-10;
+var c = c+10;
+}
+while (a <= 340){
+line (b,a,c,a)
+var a = a+10;
+var b = b+10;
+var c = c-10;
+ }
