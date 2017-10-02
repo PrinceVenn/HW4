7ea0da3606163d071b5fbda0b733f4a62322b6fd
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-10-01 18:00:40 -0700

    Create sketch-7.js

---
+++ sketch-7.js
@@ -0,0 +1,62 @@
+noFill();
+var a = 10;
+var b = 100;
+var c = 60;
+
+while (a <= 31){
+rect(a, b, c, c);
+var a = a+7;
+var b = b+8;
+var c= c-15;
+}
+
+if (a>=31){
+var a = 80;
+var b =100;
+var c = 60;
+}
+
+while (a <= 107){
+rect(a, b, c, c); 
+var a = a+7;
+var b = b+8;
+var c= c-15;
+}
+  
+if (a>=107){
+var a = 150;
+var b =100;
+var c = 60;
+}
+  
+while (a <= 171){
+rect(a, b, c, c); 
+var a = a+7;
+var b = b+8;
+var c= c-15;
+}
+
+  if (a>= 171){
+var a = 220;
+var b =100;
+var c = 60;
+  }
+
+  while (a <= 242){
+rect(a, b, c, c); 
+var a = a+7;
+var b = b+8;
+var c= c-15;
+  }
+  if (a>= 242){
+var a = 290;
+var b =100;
+var c = 60;
+  }
+  while (a <= 311){
+rect(a, b, c, c); 
+var a = a+7;
+var b = b+8;
+var c= c-15;  
+    
+}
